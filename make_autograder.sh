f394bb35492e35f3ea3a0fc51c7d93103f658e4d
Ellen Spertus <ellen.spertus@gmail.com>
2024-01-07 19:45:34 -0800

    Change path to common directory

--- make_autograder.sh
+++ make_autograder.sh
@@ -3,7 +3,7 @@
 DIRS_TO_COPY=("lib/" "config/" "gradle/" "src/")
 GRADLE_FILES=("gradlew" "build.gradle" "gradlew.bat")
 CONFIG_FILE=("config.ini")
-COMMON_DIR="../common"
+COMMON_DIR="common"
 SCRIPTS=("run_autograder" "run_autograder.py" "setup.sh")
 SCRIPT_PATHS=("${SCRIPTS[@]/#/$COMMON_DIR/}")
 
